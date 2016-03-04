9bdd9d0341b53b57b9e6354b2c9b2975d2892a46
Tim Mahrt <timmahrt@gmail.com>
2016-03-04 17:57:55 +0100

    BUGFIX: Edited get_pitch.py example to be synched with changes to main code

--- examples/get_pitch.py
+++ examples/get_pitch.py
@@ -25,22 +25,14 @@ praatEXE = r"C:\Praat.exe"
 common.makeDir(rootOutputFolder)
 
 
-pitch_and_intensity.audioToPI(wavPath, "bobby.wav", pitchPath, "bobby.txt", 
-                              praatEXE, 50, 350, forceRegenerate=False)
+bobbyPitchData = pitch_and_intensity.audioToPI(wavPath, "bobby.wav", pitchPath, 
+                                               "bobby.txt", praatEXE, 50, 350,
+                                               forceRegenerate=False)
 
-pitch_and_intensity.audioToPI(wavPath, "mary.wav", pitchPath, "mary.txt", 
-                              praatEXE, 75, 450, forceRegenerate=False)
+maryPitchData = pitch_and_intensity.audioToPI(wavPath, "mary.wav", pitchPath,
+                                              "mary.txt", praatEXE, 75, 450,
+                                              forceRegenerate=False)
 
-# Generate pitch and intensity values for all files
-# (but only "mary.wav" has a ".TextGrid" file with the same name, so this
-# will actually only generate data for one file)
-pitch_and_intensity.getPIMeasuresBatch(pitchPath, tgPath, pitchMeasuresPath,
-                                       "word",
-                                       doPitch=True, outputSuffix="batch")
-
-pitch_and_intensity.getPIMeasuresBatch(pitchPath, tgPath, rmsIntensityPath,
-                                       "word",
-                                       doPitch=False, outputSuffix="batch")
 
 # Generate pitch and intensity values for one file
 pitch_and_intensity.getPIMeasures(pitchPath, "bobby.txt", tgPath,
