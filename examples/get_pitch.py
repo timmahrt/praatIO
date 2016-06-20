ae0ac46af9c60536c7d6fedcea00cd0b2b7f0829
Tim Mahrt <timmahrt@gmail.com>
2016-06-20 11:23:51 +0200

    FEATURE: Code to extract speech formants
    Works similar to extracting pitch.

--- examples/get_pitch.py
+++ examples/get_pitch.py
@@ -10,21 +10,26 @@ import os
 from os.path import join
 
 from praatio import pitch_and_intensity
+from praatio import praat_scripts
 from praatio import tgio
-from praatio import common
+from praatio.utilities import utils
 
 wavPath = os.path.abspath(join(".", "files"))
 tgPath = os.path.abspath(join(".", "files"))
 rootOutputFolder = os.path.abspath(join(".", "files", "pitch_extraction"))
 pitchPath = join(rootOutputFolder, "pitch")
+formantsPath = join(rootOutputFolder, "formants")
 pitchMeasuresPath = join(rootOutputFolder, "pitch_measures")
 rmsIntensityPath = join(rootOutputFolder, "rms_intensity")
 
 
 praatEXE = r"C:\Praat.exe"
 #praatEXE = "/Applications/Praat.app/Contents/MacOS/Praat"
-common.makeDir(rootOutputFolder)
-
+utils.makeDir(rootOutputFolder)
+utils.makeDir(pitchPath)
+utils.makeDir(pitchMeasuresPath)
+utils.makeDir(rmsIntensityPath)
+utils.makeDir(formantsPath)
 
 bobbyPitchData = pitch_and_intensity.audioToPI(wavPath, "bobby.wav", pitchPath, 
                                                "bobby.txt", praatEXE, 50, 350,
@@ -86,3 +91,8 @@ tg.save(join(rootOutputFolder, "mary_errors.TextGrid"))
 tg = tgio.openTextGrid(join(tgPath, "mary.TextGrid"))
 tg = pitch_and_intensity.detectPitchErrors(maryFilteredPitchData, 0.75, tg)[1]
 tg.save(join(rootOutputFolder, "mary_filtered_errors.TextGrid"))
+
+formantData = praat_scripts.getFormants(praatEXE,
+                          join(wavPath, "bobby.wav"),
+                          join(formantsPath, "bobby.txt"),
+                          5500)
--- praatio/praat_scripts.py
+++ praatio/praat_scripts.py
@@ -8,6 +8,7 @@ Created on Dec 9, 2015
 @author: tmahrt
 '''
 
+import os
 from os.path import join
 
 from praatio.utilities import utils
@@ -31,6 +32,47 @@ def changeGender(praatEXE, wavFN, outputWavFN, pitchFloor, pitchCeiling,
                          [wavFN, outputWavFN, pitchFloor, pitchCeiling,
                           formantShiftRatio, pitchMedian, pitchRange,
                           duration])
+    
+
+def getFormants(praatEXE, inputWavFN, outputTxtFN, maxFormant,
+                stepSize=0.01, window_length=0.025, preemphasis=50,
+                scriptFN=None, undefinedValue=None):
+    '''
+    Get F1, F2, and F3 for the audio file
+    
+    maxFormant = 5500 for females, 5000 for males, <8000 for children
+    '''
+    if scriptFN is None:
+        scriptFN = join(utils.scriptsPath, "get_formants.praat")
+
+    argList = [inputWavFN, outputTxtFN, stepSize, maxFormant, window_length,
+               preemphasis, -1, -1]
+    utils.runPraatScript(praatEXE, scriptFN, argList)
+    
+    # Load the output
+    path, fn = os.path.split(outputTxtFN)
+    dataList = utils.openCSV(path, fn)
+
+    # The new praat script includes a header
+    if dataList[0][0] == "time":
+        dataList = dataList[1:]
+        
+    # Handle undefined values, convert values to float
+    returnList = []
+    for row in dataList:
+        keep = True
+        for i in range(1, 4):
+            if '--' in row[i]:
+                if undefinedValue is not None:
+                    row[i] = undefinedValue
+                else:
+                    keep = False
+                    break
+        
+        if keep is True:
+            returnList.append([float(val) for val in row])
+    
+    return returnList
 
 
 def resynthesizePitch(praatEXE, inputWavFN, pitchFN, outputWavFN,
