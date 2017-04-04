cc29bbd5e30599dd87b2f248196c95c77649e021
Tim Mahrt <timmahrt@gmail.com>
2017-04-04 12:19:27 +0200

    FEATURE: Example of masking segments of an audio file
    Selected segments can be replaced with a sine wave,
    effectively anonymizing the recording.

---
+++ examples/anonymize_recording.py
@@ -0,0 +1,46 @@
+'''
+Created on Apr 3, 2017
+
+@author: Tim
+
+Replaces someone saying a name with a tone over the name to 'hide' the
+name.  The times that the names are spoken are taken from a transcript
+of the recording.
+'''
+
+import os
+from os.path import join
+
+import wave
+from praatio import tgio
+from praatio import audioio
+
+path = join(".", "files")
+outputPath = join(path, "anonymized_data")
+
+if not os.path.exists(outputPath):
+    os.mkdir(outputPath)
+
+for wavFN, tgFN in (("mary.wav", "mary.TextGrid"),
+                    ("bobby.wav", "bobby_words.TextGrid")):
+    
+    outputWavFN = join(outputPath, wavFN)
+    
+    # Find the word(s) to anonymize
+    # (One could imagine a search for common names or identification of
+    # some sort of code ('section-to-anonymize') rather than what I have
+    # done here.
+    deleteList = []
+    tg = tgio.openTextGrid(join(path, tgFN))
+    deleteList.append(tg.tierDict['word'].entryList[0])
+    
+    # Get only time information from entries (i.e. remove label information)
+    deleteList = [(start, stop) for start, stop, _ in deleteList]
+    
+    # Replace segments with a sine wave
+    wavQObj = audioio.WavQueryObj(join(path, wavFN))
+    wavQObj.deleteWavSections(outputWavFN,
+                              deleteList=deleteList,
+                              operation="sine wave")
+
+
