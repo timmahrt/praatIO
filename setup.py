ca00223bce36099a75fce192f94af975d8ee5a3d
timmahrt <timmahrt@gmail.com>
2018-11-06 08:08:57 +0900

    [DOC] Update version number for release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Created on Aug 29, 2014
 from setuptools import setup
 import io
 setup(name='praatio',
-      version='3.6.12',
+      version='3.6.13',
       author='Tim Mahrt',
       author_email='timmahrt@gmail.com',
       url='https://github.com/timmahrt/praatIO',
