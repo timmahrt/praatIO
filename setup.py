6988ce27efd98f01776246afe6bd615f23644413
Tim Mahrt <timmahrt@gmail.com>
2021-04-04 19:16:26 +0900

    Format file with python Black

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 # encoding: utf-8
-'''
+"""
 Created on Aug 29, 2014
 
 @author: tmahrt
-'''
+"""
 from setuptools import setup
 import io
-setup(name='praatio',
-      version='4.2.1',
-      author='Tim Mahrt',
-      author_email='timmahrt@gmail.com',
-      url='https://github.com/timmahrt/praatIO',
-      package_dir={'praatio':'praatio'},
-      packages=['praatio',
-                'praatio.utilities'],
-      package_data={'praatio': ['praatScripts/*.praat', ]},
-      license='LICENSE',
-      description='A library for working with praat, textgrids, time aligned audio transcripts, and audio files.',
-      long_description=io.open('README.md', 'r', encoding="utf-8").read(),
-      long_description_content_type="text/markdown",
-#       install_requires=[], # No requirements! # requires 'from setuptools import setup'
-      )
+
+setup(
+    name="praatio",
+    version="4.2.1",
+    author="Tim Mahrt",
+    author_email="timmahrt@gmail.com",
+    url="https://github.com/timmahrt/praatIO",
+    package_dir={"praatio": "praatio"},
+    packages=["praatio", "praatio.utilities"],
+    package_data={
+        "praatio": [
+            "praatScripts/*.praat",
+        ]
+    },
+    license="LICENSE",
+    description="A library for working with praat, textgrids, time aligned audio transcripts, and audio files.",
+    long_description=io.open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    #       install_requires=[], # No requirements! # requires 'from setuptools import setup'
+)
