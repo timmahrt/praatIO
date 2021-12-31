a8522fec9359623f68a5e26fad763fda9faa7768
Tim Mahrt <timmahrt@gmail.com>
2021-12-31 23:00:33 +0900

    tests: add safety checks

--- tests/utilities/test_constants.py
+++ tests/utilities/test_constants.py
@@ -6,11 +6,32 @@ from tests.praatio_test_case import PraatioTestCase
 
 
 class TestConstants(PraatioTestCase):
+    def test_interval_as_named_tuple(self):
+        sut = constants.Interval(0.5, 1, "hello")
+
+        self.assertEqual(0.5, sut[0])
+        self.assertEqual(0.5, sut.start)
+
+        self.assertEqual(1, sut[1])
+        self.assertEqual(1, sut.end)
+
+        self.assertEqual("hello", sut[2])
+        self.assertEqual("hello", sut.label)
+
     def test_interval_equivalence(self):
         self.assertEqual(
             constants.Interval(0.5555555556, 1.0, "hello"),
             constants.Interval(5 / 9.0, 1.0, "hello"),
         )
+
+    def test_point_as_named_tuple(self):
+        sut = constants.Point(0.5, "hello")
+
+        self.assertEqual(0.5, sut[0])
+        self.assertEqual(0.5, sut.time)
+
+        self.assertEqual("hello", sut[1])
+        self.assertEqual("hello", sut.label)
 
     def test_point_equivalence(self):
         self.assertEqual(
