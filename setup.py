2d74475e617ed1a0912b0c9f3ef564f18c09a6a1
Tim Mahrt <timmahrt@gmail.com>
2025-11-15 16:49:49 +0900

    chore: version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import io
 setup(
     name="praatio",
     python_requires=">3.6.0",
-    version="6.2.0",
+    version="6.2.1",
     author="Tim Mahrt",
     author_email="timmahrt@gmail.com",
     url="https://github.com/timmahrt/praatIO",
